b45ef3c2bae3d59f1fbc69ab507990e966cefe39
Juniorrantila <parti.ivarsson@gmail.com>
2020-03-22 13:48:17 +0100

    .

--- currentclass.js
+++ currentclass.js
@@ -15,184 +15,222 @@ const TEACHER = 1;
 const LINK    = 2;
 
 const KLASSINFO = [
-  ["Rast", "Discord", "https://discord.gg/AZRdg4"],
-  ["Teknik","Kalles Kaviar","https://meet.google.com/toh-xdmy-kwd"],
-  ["Matte", "Björn", "https://meet.google.com/wkf-bbxo-fjv"],
-  ["Svenska", "Pontus", "https://meet.google.com/zoc-xsea-ghi"],
-  ["Samhällskunskap", "Uffe", "https://meet.google.com/nih-octd-scd"],
-  ["Historia", "Uffe", "https://meet.google.com/nih-octd-scd",],
-  ["Engelska", "Erina", "https://meet.google.com/vqn-zyxk-vme"],
-  ["Mentorstid", "Ulrika", "https://meet.google.com/iqp-evan-hir"],
-  ["Idrott", "Micke", "Finns ingen än"]
+["Rast", "Discord", "https://discord.gg/AZRdg4"],
+["Teknik","Kalles Kaviar","https://meet.google.com/toh-xdmy-kwd"],
+["Matte", "Björn", "https://meet.google.com/wkf-bbxo-fjv"],
+["Svenska", "Pontus", "https://meet.google.com/zoc-xsea-ghi"],
+["Samhällskunskap", "Uffe", "https://meet.google.com/nih-octd-scd"],
+["Historia", "Uffe", "https://meet.google.com/nih-octd-scd",],
+["Engelska", "Erina", "https://meet.google.com/vqn-zyxk-vme"],
+["Mentorstid", "Ulrika", "https://meet.google.com/iqp-evan-hir"],
+["Idrott", "Micke", "Finns ingen än"]
 ];
 
+function createE(){
+    let newElement = document.createElement('div');
+    document.body.appendChild(newElement)
+    console.dir(newElement);
+}
+
 function changeLink(url) {
-  url = classRedirect();
-  var tab = window.open(url, '_blank');
-  tab.focus();
+    url = classRedirect();
+    var tab = window.open(url, '_blank');
+    tab.focus();
 }
 
 function manRedirect(url) {
-  var tab = window.open(KLASSINFO[url][LINK], '_blank');
-  tab.focus();
+    var tab = window.open(KLASSINFO[url][LINK], '_blank');
+    tab.focus();
 }
 
 function classRedirect() {
 
-  //teknik, matte, svenska, sam, historia, engelska, mentorstid
-  var har = [];
-  var lektionStart = [];
-  var lektionEnd   = [];
+    //teknik, matte, svenska, sam, historia, engelska, mentorstid
+    var har = [];
+    var lektionStart = [];
+    var lektionEnd   = [];
 
-  var time = new Date();
-  var currentDay = time.getDay();
-  var currentHour = time.getHours();
+    var time = new Date();
+    var currentDay = time.getDay();
+    var currentHour = time.getHours();
 
-  switch(currentDay) {
+    switch(currentDay) {
 
     default:
-    for (var i = 0; i<har.length; i++){
-      har[i] = false;
-      lektionStart[i] = 0;
-      lektionEnd[i] = 0;
-    }
-    break;
+        for (var i = 0; i<har.length; i++){
+            har[i] = false;
+            lektionStart[i] = 0;
+            lektionEnd[i] = 0;
+        }
+        break;
 
     case 1:
-    har[TEKNIK]  = true;
-    har[MATTE]   = false;
-    har[SVENSKA] = false;
-    har[SAM]     = true;
-    har[HISTORIA]= false;
-    har[ENGELSKA]= false;
-    har[MENTOR]  = false;
-    har[IDROTT]  = true;
+        har[TEKNIK]  = true;
+        har[MATTE]   = false;
+        har[SVENSKA] = false;
+        har[SAM]     = true;
+        har[HISTORIA]= false;
+        har[ENGELSKA]= false;
+        har[MENTOR]  = false;
+        har[IDROTT]  = true;
 
-    lektionStart[TEKNIK] = 8;
-    lektionEnd[TEKNIK]   = 9;
+        lektionStart[TEKNIK] = 8;
+        lektionEnd[TEKNIK]   = 9;
 
-    lektionStart[IDROTT] = 10;
-    lektionEnd[IDROTT]   = 11;
+        lektionStart[IDROTT] = 10;
+        lektionEnd[IDROTT]   = 11;
 
-    lektionStart[SAM]    = 12;
-    lektionEnd[SAM]      = 13;
+        lektionStart[SAM]    = 12;
+        lektionEnd[SAM]      = 13;
 
-    break;
+        break;
 
     case 2:
-    har[TEKNIK]   = true;
-    har[MATTE]    = true;
-    har[SVENSKA]  = true;
-    har[SAM]      = false;
-    har[HISTORIA] = false;
-    har[ENGELSKA] = false;
-    har[MENTOR]   = false;
+        har[TEKNIK]   = true;
+        har[MATTE]    = true;
+        har[SVENSKA]  = true;
+        har[SAM]      = false;
+        har[HISTORIA] = false;
+        har[ENGELSKA] = false;
+        har[MENTOR]   = false;
 
-    lektionStart[TEKNIK]  = 9;
-    lektionEnd[TEKNIK]    = 11;
+        lektionStart[TEKNIK]  = 9;
+        lektionEnd[TEKNIK]    = 11;
 
-    lektionStart[SVENSKA] = 12;
-    lektionEnd[SVENSKA]   = 13;
+        lektionStart[SVENSKA] = 12;
+        lektionEnd[SVENSKA]   = 13;
 
-    lektionStart[MATTE]   = 13;
-    lektionEnd[MATTE]     = 16;
+        lektionStart[MATTE]   = 13;
+        lektionEnd[MATTE]     = 16;
 
-    break;
+        break;
 
     case 3:
-    har[TEKNIK]   = false;
-    har[MATTE]    = true;
-    har[SVENSKA]  = true;
-    har[SAM]      = false;
-    har[HISTORIA] = false;
-    har[ENGELSKA] = false;
-    har[MENTOR]   = false;
-    har[IDROTT]   = true;
+        har[TEKNIK]   = false;
+        har[MATTE]    = true;
+        har[SVENSKA]  = true;
+        har[SAM]      = false;
+        har[HISTORIA] = false;
+        har[ENGELSKA] = false;
+        har[MENTOR]   = false;
+        har[IDROTT]   = true;
 
-    lektionStart[MATTE]   = 8
-    lektionEnd[MATTE]     = 9
+        lektionStart[MATTE]   = 8
+        lektionEnd[MATTE]     = 9
 
-    lektionStart[SVENSKA] = 9
-    lektionEnd[SVENSKA]   = 11
+        lektionStart[SVENSKA] = 9
+        lektionEnd[SVENSKA]   = 11
 
-    lektionStart[IDROTT]  = 13
-    lektionEnd[IDROTT]    = 14
+        lektionStart[IDROTT]  = 13
+        lektionEnd[IDROTT]    = 14
 
-    break;
+        break;
 
     case 4:
-    har[TEKNIK]  = true;
-    har[MATTE]   = false;
-    har[SVENSKA] = false;
-    har[SAM]     = true;
-    har[HISTORIA]= true;
-    har[ENGELSKA]= true;
-    har[MENTOR]  = false;
+        har[TEKNIK]  = true;
+        har[MATTE]   = false;
+        har[SVENSKA] = false;
+        har[SAM]     = true;
+        har[HISTORIA]= true;
+        har[ENGELSKA]= true;
+        har[MENTOR]  = false;
 
-    lektionStart[HISTORIA] = 9;
-    lektionEnd[HISTORIA]   = 11;
+        lektionStart[HISTORIA] = 9;
+        lektionEnd[HISTORIA]   = 11;
 
-    lektionStart[SAM]      = 12;
-    lektionEnd[SAM]        = 13;
+        lektionStart[SAM]      = 12;
+        lektionEnd[SAM]        = 13;
 
-    lektionStart[ENGELSKA] = 13;
-    lektionEnd[ENGELSKA]   = 15;
+        lektionStart[ENGELSKA] = 13;
+        lektionEnd[ENGELSKA]   = 15;
 
-    lektionStart[TEKNIK]   = 15;
-    lektionEnd[TEKNIK]     = 16;
+        lektionStart[TEKNIK]   = 15;
+        lektionEnd[TEKNIK]     = 16;
 
-    break;
+        break;
 
     case 5:
-    har[TEKNIK]  = false;
-    har[MATTE]   = true;
-    har[SVENSKA] = false;
-    har[SAM]     = false;
-    har[HISTORIA]= true;
-    har[ENGELSKA]= true;
-    har[MENTOR]  = true;
+        har[TEKNIK]  = false;
+        har[MATTE]   = true;
+        har[SVENSKA] = false;
+        har[SAM]     = false;
+        har[HISTORIA]= true;
+        har[ENGELSKA]= true;
+        har[MENTOR]  = true;
 
-    lektionStart[MATTE]    = 8;
-    lektionEnd[MATTE]      = 9;
+        lektionStart[MATTE]    = 8;
+        lektionEnd[MATTE]      = 9;
 
-    lektionStart[HISTORIA] = 10;
-    lektionEnd[HISTORIA]   = 11;
+        lektionStart[HISTORIA] = 10;
+        lektionEnd[HISTORIA]   = 11;
 
-    lektionStart[MENTOR]   = 12;
-    lektionEnd[MENTOR]     = 13;
+        lektionStart[MENTOR]   = 12;
+        lektionEnd[MENTOR]     = 13;
 
-    lektionStart[ENGELSKA] = 13;
-    lektionEnd[ENGELSKA]   = 15;
+        lektionStart[ENGELSKA] = 13;
+        lektionEnd[ENGELSKA]   = 15;
 
-    break;
-  }
+        break;
+    }
 
-  var lektion = RAST;
-  for (var i = 0; i<lektionStart.length; i++){
-    if (currentHour >= lektionStart[i] && currentHour <= lektionEnd[i]) lektion = i;
-  }
+    var lektion = RAST;
+    for (var i = 0; i<lektionStart.length; i++){
+        if (currentHour >= lektionStart[i] && currentHour <= lektionEnd[i]) lektion = i;
+    }
 
-  var ln, tn , link, goTo;
+    var ln, tn , link, goTo;
 
-  lecture   = KLASSINFO[lektion][LECTURE];
-  teacher   = KLASSINFO[lektion][TEACHER];
-  link      = KLASSINFO[lektion][LINK];
-  if (lektion == RAST) goTo = "Gå på rast";
-  else goTo = "Gå till " + lecture + " meet";
+    lecture   = KLASSINFO[lektion][LECTURE];
+    teacher   = KLASSINFO[lektion][TEACHER];
+    link      = KLASSINFO[lektion][LINK];
+    if (lektion == RAST) goTo = "Gå på rast";
+    else goTo = "Gå till " + lecture + " meet";
 
-  document.getElementById("lecture").innerHTML  = lecture;
-  document.getElementById("teacher").innerHTML  = teacher;
-  document.getElementById("link").innerHTML     = link;
-  document.getElementById("GOTO").innerHTML     = goTo;
+    document.getElementById("lecture").innerHTML  = lecture;
+    document.getElementById("teacher").innerHTML  = teacher;
+    document.getElementById("link").innerHTML     = link;
+    document.getElementById("GOTO").innerHTML     = goTo;
 
 
-  //Set all of class lists
-  var antalLektioner = 8;
-  for (var i = 0; i<antalLektioner; i++){
-    document.getElementById("lektionId"+i).innerHTML = KLASSINFO[i+1][LECTURE];
-    document.getElementById("teacherId"+i).innerHTML = KLASSINFO[i+1][TEACHER];
-    document.getElementById("linkId"+i).innerHTML    = KLASSINFO[i+1][LINK];
-  }
+    const table = document.getElementById("table");
+    const BUTTON = 3;
 
-  return link;
+    //Set all of class lists
+    var k = 0;
+    var antalLektioner = 8;
+    for (var i = 0; i<antalLektioner; i++){
+        let newElement = document.createElement('tr');
+        newElement.id = 'row'+i;
+        table.appendChild(newElement);
+        
+        for (var j = 0; j<4; j++){
+            const row = document.getElementById('row'+i);
+            newElement = document.createElement('td');
+            newElement.id = 'col'+k;
+            switch(j){
+                case(LECTURE):
+                newElement.textContent = KLASSINFO[i+1][LECTURE];
+                break;
+                
+                case(TEACHER):
+                newElement.textContent = KLASSINFO[i+1][TEACHER];
+                break;
+                
+                case(LINK):
+                newElement.textContent = KLASSINFO[i+1][LINK];
+                break;
+                
+                case(BUTTON):
+                let newButton = document.getElementById('col'+k);
+                const buttons = document.createElement('button');
+                newButton.appendChild(buttons);
+                //newElement.textContent = 'd';
+                break;
+            }
+            row.appendChild(newElement);
+
+        }
+    }
+
+    return link;
 }
