2ba1226ed173fb8a4341e8a6fba8b1dada16d196
Juniorrantila <parti.ivarsson@gmail.com>
2020-03-27 11:39:05 +0100

    .

--- currentclass.js
+++ currentclass.js
@@ -1,130 +1,231 @@
+const RAST     = -1;
+const TEKNIK   = 0;
+const MATTE    = 1;
+const SVENSKA  = 2;
+const SAM      = 3;
+const HISTORIA = 4;
+const ENGELSKA = 5;
+const MENTOR   = 6;
+const IDROTT   = 7;
 
-let iCalURL = localStorage.getItem("iCalURL");
+const LECTURE = 0;
+const TEACHER = 1;
+const LINK    = 2;
 
+var lecture, teacher, link;
 
-let link = "";
-
-if (!localStorage.getItem("lectureStartOffset")) localStorage.setItem("lectureStartOffset", 600000)
-
-const MEET = [
-    {subject: "TEKTEK01",   meet: "https://meet.google.com/sgi-imkg-yqv"},
-    {subject: "MATMAT02c",   meet: "https://meet.google.com/wkf-bbxo-fjv"},
-    {subject: "SVESVE01",   meet: "https://meet.google.com/zoc-xsea-ghi"},
-    {subject: "SVASVA01",   meet: "https://meet.google.com/dzw-ejay-sgp"},
-    {subject: "SAMSAM01b",  meet: "https://meet.google.com/pwg-znqn-ebe"},
-    {subject: "HISHIS01a1", meet: "https://meet.google.com/nih-octd-scd"},
-    {subject: "ENGENG05",   meet: "https://meet.google.com/vqn-zyxk-vme"},
-    {subject: "MENTORSTID", meet: "https://meet.google.com/iqp-evan-hir"},
-    {subject: "IDRIDR01",   meet: "https://meet.google.com/dxk-ngzd-pgf"}
+const KLASSINFO = [
+["Teknik","Kalles Kaviar","https://meet.google.com/sgi-imkg-yqv"],
+["Matte", "Björn", "https://meet.google.com/wkf-bbxo-fjv"],
+["Svenska", "Pontus", "https://meet.google.com/zoc-xsea-ghi"],
+["Samhällskunskap", "Uffe", "https://meet.google.com/pwg-znqn-ebe"],
+["Historia", "Uffe", "https://meet.google.com/nih-octd-scd",],
+["Engelska", "Erina", "https://meet.google.com/vqn-zyxk-vme"],
+["Mentorstid", "Ulrika", "https://meet.google.com/iqp-evan-hir"],
+["Idrott", "Micke", "https://meet.google.com/dxk-ngzd-pgf"]
 ];
 
 const RASTINFO = ["Rast", "Discord", "https://discord.gg/HShBsv6"];
 
-function isWithinTimeSpan(date, day, time) {
-    days = ["S\u00F6ndag", "M\u00E5ndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "L\u00F6rdag"];
-    if (day != days[date.getDay()]) return false;
-    
-    let offset = localStorage.getItem("lectureStartOffset"); //default 10min
-    
-    let startTime = new Date(date).setHours(parseInt(time.split("-")[0].split(":")[0]), parseInt(time.split("-")[0].split(":")[1]), 0, 0);
-    startTime -= offset;
-    let endTime = new Date(date).setHours(parseInt(time.split("-")[1].split(":")[0]), parseInt(time.split("-")[1].split(":")[1]), 0, 0);
-    
-    return date >= startTime && date < endTime;
+function openLink() {
+    var tab = window.open(link, '_blank');
+    tab.focus();
 }
 
-if (iCalURL != null){
-    //run ical parser on load
-    window.addEventListener("load", function() {
-        //Create new ical parser
-        new ical_parser(iCalURL, function(cal) {
-            //When ical parser has loaded file
-            //get future events
-            events = cal.getFutureEvents();
-            schema.reverse();
-            
-            let currentLectures = 0;
-            //let date = new Date("March 25, 2020 09:40");
-            let date = new Date();
-            
-            schema.forEach(function(e, i) {
-                for (j = 0; j < MEET.length; j++) {
-                    if (e[2] == MEET[j].subject) schema[i].push(MEET[j].meet);
-                }
-                if (isWithinTimeSpan(date, e[0], e[1])) {
-                    currentLectures++;
-                    tr = document.getElementById("currentLectures").insertRow();
-
-                    for (let j = 0; j < 3; j++) {
-                        let td = tr.insertCell();
-                        td.textContent = e[j + 2] ? e[j + 2] : "-";
-                    }
-                    if (e[4]) {
-                        link = e[4];
-                        let td = tr.insertCell();
-                        
-                        td.appendChild(redir = document.createElement("button"));
-                        redir.setAttribute("class", "btn btn-danger");
-                        redir.setAttribute("onclick", "openLink()");
-                        redir.setAttribute("id", "GOTO");
-                        redir.textContent = "G\u00E5 till "+ schema[i][2].toLowerCase() + " meet";
-                    }
-                }
-            });
-
-            // update current lectures
-            if (currentLectures <= 0) {
-                tr = document.getElementById("currentLectures").insertRow();
-                for (i = 0; i < 3; i++) {
-                    let td = tr.insertCell();
-                    td.textContent = RASTINFO[i];
-                }
-                link = RASTINFO[2];
-                let td = tr.insertCell();
-                td.appendChild(redir = document.createElement("button"));
-                redir.setAttribute("class", "btn btn-danger");
-                redir.setAttribute("onclick", "openLink()");
-                redir.setAttribute("id", "GOTO");
-                redir.textContent = "G\u00E5 p\u00E5 rast";
-                document.getElementById("GOTO").textContent = "G\u00E5 p\u00E5 rast";
-            }
-
-            // create table
-            let tblbody = document.getElementById("table")
-            for (let i = 0; i < schema.length; i++) {
-                let tr = tblbody.insertRow();
-                if (schema[i][0] != days[date.getDay()]) continue;
-                for (let j = 0; j < 4; j++) {
-                    let td = tr.insertCell();
-                    td.textContent = schema[i][j] ? schema[i][j] : "-";
-                }
-                if (schema[i][4]) {
-                    let td = tr.insertCell();
-                    td.appendChild(anchor = document.createElement("a"));
-                    anchor.textContent = schema[i][4];
-                    anchor.setAttribute("href", schema[i][4])
-                } else {
-                    let td = tr.insertCell();
-                    td.textContent = "-";
-                }
-            }
-        });
-    }, false);
+function manRedirect(url) {
+    var tab = window.open(KLASSINFO[url][LINK], '_blank');
+    tab.focus();
 }
 
 function classRedirect() {
 
-    document.getElementById("iCalURL").value = iCalURL;
+    //teknik, matte, svenska, sam, historia, engelska, mentorstid
+    var har = [];
+    har.length = 8;
+    var lektionStart = [];
+    var lektionEnd   = [];
 
-}
+    var time = new Date();
+    var currentDay = time.getDay();
+    var currentHour = time.getHours();
 
-function submitICal() {
-    iCalURL = document.getElementById("iCalURL").value;
-    localStorage.setItem("iCalURL", iCalURL);
-    window.location.reload();
-}
+    switch(currentDay) {
 
-function openLink() {
-    let tab = window.open(link, '_blank');
-    tab.focus();
+    default:
+        for (var i = 0; i<har.length; i++){
+            har[i] = false;
+            lektionStart[i] = 0;
+            lektionEnd[i] = 0;
+        }
+        break;
+
+    case 1:
+        har[TEKNIK]  = true;
+        har[MATTE]   = false;
+        har[SVENSKA] = false;
+        har[SAM]     = true;
+        har[HISTORIA]= false;
+        har[ENGELSKA]= false;
+        har[MENTOR]  = false;
+        har[IDROTT]  = true;
+
+        lektionStart[TEKNIK] = 8;
+        lektionEnd[TEKNIK]   = 9;
+
+        lektionStart[IDROTT] = 9;
+        lektionEnd[IDROTT]   = 11;
+
+        lektionStart[SAM]    = 12;
+        lektionEnd[SAM]      = 13;
+
+        break;
+
+    case 2:
+        har[TEKNIK]   = true;
+        har[MATTE]    = true;
+        har[SVENSKA]  = true;
+        har[SAM]      = false;
+        har[HISTORIA] = false;
+        har[ENGELSKA] = false;
+        har[MENTOR]   = false;
+
+        lektionStart[TEKNIK]  = 9;
+        lektionEnd[TEKNIK]    = 11;
+
+        lektionStart[SVENSKA] = 12;
+        lektionEnd[SVENSKA]   = 13;
+
+        lektionStart[MATTE]   = 13;
+        lektionEnd[MATTE]     = 16;
+
+        break;
+
+    case 3:
+        har[TEKNIK]   = false;
+        har[MATTE]    = true;
+        har[SVENSKA]  = true;
+        har[SAM]      = false;
+        har[HISTORIA] = false;
+        har[ENGELSKA] = false;
+        har[MENTOR]   = false;
+        har[IDROTT]   = true;
+
+        lektionStart[MATTE]   = 8
+        lektionEnd[MATTE]     = 9
+
+        lektionStart[SVENSKA] = 9
+        lektionEnd[SVENSKA]   = 11
+
+        lektionStart[IDROTT]  = 13
+        lektionEnd[IDROTT]    = 14
+
+        break;
+
+    case 4:
+        har[TEKNIK]  = true;
+        har[MATTE]   = false;
+        har[SVENSKA] = false;
+        har[SAM]     = true;
+        har[HISTORIA]= true;
+        har[ENGELSKA]= true;
+        har[MENTOR]  = false;
+
+        lektionStart[HISTORIA] = 9;
+        lektionEnd[HISTORIA]   = 11;
+
+        lektionStart[SAM]      = 12;
+        lektionEnd[SAM]        = 13;
+
+        lektionStart[ENGELSKA] = 13;
+        lektionEnd[ENGELSKA]   = 15;
+
+        lektionStart[TEKNIK]   = 15;
+        lektionEnd[TEKNIK]     = 16;
+
+        break;
+
+    case 5:
+        har[TEKNIK]  = false;
+        har[MATTE]   = true;
+        har[SVENSKA] = false;
+        har[SAM]     = false;
+        har[HISTORIA]= true;
+        har[ENGELSKA]= true;
+        har[MENTOR]  = true;
+
+        lektionStart[MATTE]    = 8;
+        lektionEnd[MATTE]      = 9;
+
+        lektionStart[HISTORIA] = 9;
+        lektionEnd[HISTORIA]   = 11;
+
+        lektionStart[MENTOR]   = 12;
+        lektionEnd[MENTOR]     = 13;
+
+        lektionStart[ENGELSKA] = 13;
+        lektionEnd[ENGELSKA]   = 15;
+
+        break;
+    }
+
+    var lektion = RAST;
+    for (var i = 0; i<lektionStart.length; i++){
+        if (currentHour >= lektionStart[i] && currentHour < lektionEnd[i]) lektion = i;
+    }
+
+    var goTo;
+
+
+    if (lektion == RAST) {
+        lecture   = RASTINFO[LECTURE];
+        teacher   = RASTINFO[TEACHER]
+        link      = RASTINFO[LINK];
+        goTo = "Gå på rast";
+    }
+    else {
+        lecture   = KLASSINFO[lektion][LECTURE];
+        teacher   = KLASSINFO[lektion][TEACHER];
+        link      = KLASSINFO[lektion][LINK];
+        goTo = "Gå till " + lecture + " meet";
+    }
+
+    document.getElementById("lecture").textContent  = lecture;
+    document.getElementById("teacher").textContent  = teacher;
+    document.getElementById("link").textContent     = link;
+    document.getElementById("GOTO").textContent     = goTo;
+
+    //Set all of class lists
+    const table = document.getElementById("table");
+    for (let i = 0; i<har.length; i++){
+        let newRow = document.createElement('tr');
+        table.appendChild(newRow);
+        let row = table.getElementsByTagName("tr");
+
+        for (let j = 0; j<KLASSINFO[i].length+1; j++){
+            newTd = document.createElement('td');
+            newAnchor = document.createElement('a');
+            switch(j){
+                case(LECTURE):
+                newTd.textContent = KLASSINFO[i][LECTURE];
+                break;
+
+                case(TEACHER):
+                newTd.textContent = KLASSINFO[i][TEACHER];
+                break;
+
+                case(LINK):
+                let anchorElem = document.createElement('a');
+                anchorElem.setAttribute("href", KLASSINFO[i][LINK]);
+                anchorElem.innerHTML = KLASSINFO[i][LINK];
+                newTd.appendChild(anchorElem); // append your new link to the body
+                break;
+
+                case(3):
+                if (lektionStart[i] != undefined) newTd.textContent = lektionStart[i] + ' - ' + lektionEnd[i];
+                else newTd.textContent = "Ingen " + KLASSINFO[i][LECTURE] + " idag.";
+
+            }
+            row[i].appendChild(newTd);
+        }
+    }
 }
---
+++ iCal/currentclass.js
@@ -0,0 +1,130 @@
+
+let iCalURL = localStorage.getItem("iCalURL");
+
+
+let link = "";
+
+if (!localStorage.getItem("lectureStartOffset")) localStorage.setItem("lectureStartOffset", 600000)
+
+const MEET = [
+    {subject: "TEKTEK01",   meet: "https://meet.google.com/sgi-imkg-yqv"},
+    {subject: "MATMAT02c",   meet: "https://meet.google.com/wkf-bbxo-fjv"},
+    {subject: "SVESVE01",   meet: "https://meet.google.com/zoc-xsea-ghi"},
+    {subject: "SVASVA01",   meet: "https://meet.google.com/dzw-ejay-sgp"},
+    {subject: "SAMSAM01b",  meet: "https://meet.google.com/pwg-znqn-ebe"},
+    {subject: "HISHIS01a1", meet: "https://meet.google.com/nih-octd-scd"},
+    {subject: "ENGENG05",   meet: "https://meet.google.com/vqn-zyxk-vme"},
+    {subject: "MENTORSTID", meet: "https://meet.google.com/iqp-evan-hir"},
+    {subject: "IDRIDR01",   meet: "https://meet.google.com/dxk-ngzd-pgf"}
+];
+
+const RASTINFO = ["Rast", "Discord", "https://discord.gg/HShBsv6"];
+
+function isWithinTimeSpan(date, day, time) {
+    days = ["S\u00F6ndag", "M\u00E5ndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "L\u00F6rdag"];
+    if (day != days[date.getDay()]) return false;
+    
+    let offset = localStorage.getItem("lectureStartOffset"); //default 10min
+    
+    let startTime = new Date(date).setHours(parseInt(time.split("-")[0].split(":")[0]), parseInt(time.split("-")[0].split(":")[1]), 0, 0);
+    startTime -= offset;
+    let endTime = new Date(date).setHours(parseInt(time.split("-")[1].split(":")[0]), parseInt(time.split("-")[1].split(":")[1]), 0, 0);
+    
+    return date >= startTime && date < endTime;
+}
+
+if (iCalURL != null){
+    //run ical parser on load
+    window.addEventListener("load", function() {
+        //Create new ical parser
+        new ical_parser(iCalURL, function(cal) {
+            //When ical parser has loaded file
+            //get future events
+            events = cal.getFutureEvents();
+            schema.reverse();
+            
+            let currentLectures = 0;
+            //let date = new Date("March 25, 2020 09:40");
+            let date = new Date();
+            
+            schema.forEach(function(e, i) {
+                for (j = 0; j < MEET.length; j++) {
+                    if (e[2] == MEET[j].subject) schema[i].push(MEET[j].meet);
+                }
+                if (isWithinTimeSpan(date, e[0], e[1])) {
+                    currentLectures++;
+                    tr = document.getElementById("currentLectures").insertRow();
+
+                    for (let j = 0; j < 3; j++) {
+                        let td = tr.insertCell();
+                        td.textContent = e[j + 2] ? e[j + 2] : "-";
+                    }
+                    if (e[4]) {
+                        link = e[4];
+                        let td = tr.insertCell();
+                        
+                        td.appendChild(redir = document.createElement("button"));
+                        redir.setAttribute("class", "btn btn-danger");
+                        redir.setAttribute("onclick", "openLink()");
+                        redir.setAttribute("id", "GOTO");
+                        redir.textContent = "G\u00E5 till "+ schema[i][2].toLowerCase() + " meet";
+                    }
+                }
+            });
+
+            // update current lectures
+            if (currentLectures <= 0) {
+                tr = document.getElementById("currentLectures").insertRow();
+                for (i = 0; i < 3; i++) {
+                    let td = tr.insertCell();
+                    td.textContent = RASTINFO[i];
+                }
+                link = RASTINFO[2];
+                let td = tr.insertCell();
+                td.appendChild(redir = document.createElement("button"));
+                redir.setAttribute("class", "btn btn-danger");
+                redir.setAttribute("onclick", "openLink()");
+                redir.setAttribute("id", "GOTO");
+                redir.textContent = "G\u00E5 p\u00E5 rast";
+                document.getElementById("GOTO").textContent = "G\u00E5 p\u00E5 rast";
+            }
+
+            // create table
+            let tblbody = document.getElementById("table")
+            for (let i = 0; i < schema.length; i++) {
+                let tr = tblbody.insertRow();
+                if (schema[i][0] != days[date.getDay()]) continue;
+                for (let j = 0; j < 4; j++) {
+                    let td = tr.insertCell();
+                    td.textContent = schema[i][j] ? schema[i][j] : "-";
+                }
+                if (schema[i][4]) {
+                    let td = tr.insertCell();
+                    td.appendChild(anchor = document.createElement("a"));
+                    anchor.textContent = schema[i][4];
+                    anchor.setAttribute("href", schema[i][4])
+                } else {
+                    let td = tr.insertCell();
+                    td.textContent = "-";
+                }
+            }
+        });
+    }, false);
+}
+
+function classRedirect() {
+
+    document.getElementById("iCalURL").value = iCalURL;
+
+}
+
+function submitICal() {
+    iCalURL = document.getElementById("iCalURL").value;
+    localStorage.setItem("iCalURL", iCalURL);
+    window.location.reload();
+}
+
+function openLink() {
+    let tab = window.open(link, '_blank');
+    tab.focus();
+}
